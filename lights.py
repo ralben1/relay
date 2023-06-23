1bc9202981aadc59b20102baa324e4f26ec76c13
ralben1 <89206732+ralben1@users.noreply.github.com>
2023-06-23 10:00:49 +0200

    Update lights.py

--- lights.py
+++ lights.py
@@ -8,14 +8,20 @@ class Light():
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(relay1, GPIO.OUT)
         GPIO.setup(relay2, GPIO.OUT)
+        GPIO.output(relay1, GPIO.HIGH)
+        GPIO.output(relay2, GPIO.HIGH)
     def light1on(self):
+        GPIO.output(relay1, GPIO.LOW)
         return "Light 1 on"
 
     def light1off(self):
         return "Light 1 off"
+        GPIO.output(relay1, GPIO.HIGH)
 
     def light2on(self):
+        GPIO.output(relay2, GPIO.LOW)
         return "Light 2 on"
 
     def light2off(self):
         return "Light 2 off"
+        GPIO.output(relay2, GPIO.HIGH)
